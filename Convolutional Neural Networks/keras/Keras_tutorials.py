30d94b651f4cd4d233698e382cd7bf70084cd582
predator4hack <chandankum2440@gmail.com>
2020-10-07 17:00:04 +0530

    load file

---
+++ Convolutional Neural Networks/keras/Keras_tutorials.py
@@ -0,0 +1,34 @@
+import numpy as np
+from keras import layers
+from keras.layers import Input, ZeroPadding1D, Dense, BatchNormalization, Flatten, Conv2D
+from keras.layers import AveragePooling2D, MaxPool2D, Dropout, GlobalAveragePooling2D, GlobalMaxPooling2D
+from keras.models import Model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from kt_utils import *
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+X_train = X_test_orig/255
+X_test = X_test_orig/255
+
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
