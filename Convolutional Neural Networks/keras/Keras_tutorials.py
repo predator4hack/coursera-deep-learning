2cafd3240a5f5d68d8bad1b54621611943c62739
predator4hack <chandankum2440@gmail.com>
2020-10-08 10:38:54 +0530

    model completed with 99.97 accuracy

--- Convolutional Neural Networks/keras/Keras_tutorials.py
+++ Convolutional Neural Networks/keras/Keras_tutorials.py
@@ -1,6 +1,6 @@
 import numpy as np
 from keras import layers
-from keras.layers import Input, ZeroPadding1D, Dense, BatchNormalization, Flatten, Conv2D
+from keras.layers import Input, ZeroPadding2D, Dense, BatchNormalization, Flatten, Conv2D, Activation
 from keras.layers import AveragePooling2D, MaxPool2D, Dropout, GlobalAveragePooling2D, GlobalMaxPooling2D
 from keras.models import Model
 from keras.preprocessing import image
@@ -20,8 +20,8 @@ from matplotlib.pyplot import imshow
 
 X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
 
-X_train = X_test_orig/255
-X_test = X_test_orig/255
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
 
 Y_train = Y_train_orig.T
 Y_test = Y_test_orig.T
@@ -32,3 +32,23 @@ print ("X_train shape: " + str(X_train.shape))
 print ("Y_train shape: " + str(Y_train.shape))
 print ("X_test shape: " + str(X_test.shape))
 print ("Y_test shape: " + str(Y_test.shape))
+
+def HappyModel(input_shape):
+    X_input = Input(input_shape)
+
+    X = ZeroPadding2D((3,3))(X_input)
+    X = Conv2D(32, (7, 7), strides=(1, 1), name='conv0')(X)
+    X = BatchNormalization(axis = 3, name= 'bn0')(X)
+    X = Activation('relu')(X)
+
+    X = MaxPool2D((2, 2), name = 'max_pool')(X)
+    X = Flatten()(X)
+    X = Dense(1, activation='sigmoid', name='fc')(X)
+
+    model = Model(input = X_input, outputs = X, name='HappyModel')
+
+    return model
+
+happymodel = HappyModel(X_train.shape[1:])
+happymodel.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+happymodel.fit(X_train, Y_train, epochs=40, batch_size=50)
